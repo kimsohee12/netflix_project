2584d529380e0a4feca67a7fa6a59b96c54941dd
김소희 <130417959+kimsohee12@users.noreply.github.com>
2023-08-05 18:43:08 +0900

    메뉴바 , CSS, 부트스트랩

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home'
 import Movies from './pages/Movies'
 import MovieDetail from './pages/MovieDetail'
+import Navigation from './components/Navigation';
+
 
 //1. 3개의페이지 필요 - 홈페이지, movie페이지, movieDetail페이지
 //2. 홈페이지에서 배너를 볼 수 있다 
@@ -21,10 +24,12 @@ import MovieDetail from './pages/MovieDetail'
 function App() {
   return (
     <div>
+      <Navigation/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/movies' element={<Movies/>}/>
         <Route path='/movies/:id' element={<MovieDetail/>}/>
+        ;;
       </Routes>
     </div>
   );
---
+++ src/components/Navigation.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import{Form, Button , FormControl} from 'react-bootstrap';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import NLogo from '../imgs/netflix_logo.png';
+import { Link } from 'react-router-dom';
+
+
+const Navigation = () => {
+  return (
+    <>
+    
+    <Navbar expand="lg" bg="dark" data-bs-theme="dark" >
+        <Container fluid>
+            <Navbar.Brand href="/"><img width={'90px'} src={NLogo} className='logoImg'/></Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll"/>
+            <Navbar.Collapse id="navbarScroll">
+            <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+                >
+                <Link to="/" className='nav_item'>Home</Link>
+                <Link to="movies" className='nav_item'>movies</Link>
+                
+            </Nav>
+            <Form className="d-flex">
+                <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                />
+                <Button variant="outline-danger" style={{width:'90px'}}>검색</Button>
+            </Form>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+    </>
+  );
+}
+        
+
+export default Navigation
