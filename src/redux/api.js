9e349e55326af57efe8491597185f9823010be4e
김소희 <130417959+kimsohee12@users.noreply.github.com>
2023-08-05 21:01:10 +0900

    reduecer , Promise로 api 동시에 불러오도록 적용 ,API 세팅

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,24 +1,32 @@
 import axios from "axios";
 
+//기본 베이스되는 URL 지정 
 const api = axios.create({
     baseURL : "https://api.themoviedb.org/3",
     headers : {"Content-Type" : "application/json"}
 
 });
 
+
+//req
 api.interceptors.request.use(function(config){
     console.log("request start", config);
     return config;
-},function(error){
+},
+function(error){
     console.log("request err", error);
     return Promise.reject(error);
 });
-
+//res -> axios 사용하면 data 라는 필드 안에 받은 응답을 넣어줌 
 api.interceptors.response.use(function(response){
     console.log("get response", response);
     return response;
-},function(error){
+},
+function(error){
     console.log("response err", error);
     return Promise.reject(error);
 });
+
+
+
 export default api
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import {combineReducers}from 'redux';
+import movieReducer from './movieReducer';
 
 export default combineReducers({
-    
+    move:movieReducer, //-> combineReducers 를 통해 store한테 리듀서 전달
 })
